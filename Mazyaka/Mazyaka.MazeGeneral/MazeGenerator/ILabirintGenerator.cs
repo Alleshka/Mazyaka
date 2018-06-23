ed31e1067a7311860f4059b1d0551fd9382afd03
grebovod_aleksei@mail.ru <grebovod_aleksei@mail.ru>
2018-06-23 20:07:11 +0300

    Start rework

--- Mazyaka/Mazyaka.MazeGeneral/MazeGenerator/ILabirintGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mazyaka.MazeGeneral.MazeModel;
-
-namespace Mazyaka.MazeGeneral.MazeGenerator
-{
-    public interface ILabirintGenerator
-    {
-        MazeStruct Generate(int size);
-    }
-}
