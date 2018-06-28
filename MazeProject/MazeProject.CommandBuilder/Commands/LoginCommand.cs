c5d3af12c4a1c4a060372a9598c39831ffcee7e2
grebovod_aleksei@mail.ru <grebovod_aleksei@mail.ru>
2018-06-29 00:36:39 +0300

    login

---
+++ MazeProject/MazeProject.CommandBuilder/Commands/LoginCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MazeProject.General.Package;
+using MazeProject.LoginService;
+using MazeProject.MessageSender;
+using System.Net.Sockets;
+
+namespace MazeProject.CommandBuilder.Commands
+{
+    public class LoginCommand : BaseCommand
+    {
+        private ILoginService loginService;
+        private MessageSender.MessageSender messageSender;
+        private Socket socket;
+
+        public LoginCommand(BasePackage package, ILoginService loginService, MessageSender.MessageSender sender, Socket userSocket) : base(package)
+        {
+            this.loginService = loginService;
+            this.messageSender = sender;
+            this.socket = userSocket;
+        }
+
+        public override Tuple<List<Guid>, string> Execute()
+        {
+            LoginRequest loginRequest = this.Request as LoginRequest;
+            Guid userID = loginService.Login(loginRequest.Login, loginRequest.Passwod);
+            messageSender.AddUser(socket, userID);
+            LoginResponse loginResponse = new LoginResponse(loginRequest.Login, userID);
+            return new Tuple<List<Guid>, string>(new List<Guid>() { userID }, loginResponse.ToString());
+        }
+    }
+}
---
+++ MazeProject/MazeProject.General/Package/Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MazeProject.General.Package
+{
+    public class LoginRequest : BaseRequest
+    {
+        public String Login { get; private set; }
+        public String Passwod { get; private set; }
+
+        public LoginRequest (String login, String password) : base("Login")
+        {
+            this.Login = login;
+            this.Passwod = password;
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class LoginResponse : BaseResponse
+    {
+        public String Login { get; private set; }
+        public Guid ID { get; private set; }
+
+        public LoginResponse(String login, Guid id) : base("Login")
+        {
+            this.Login = login;
+            this.ID = id;
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
