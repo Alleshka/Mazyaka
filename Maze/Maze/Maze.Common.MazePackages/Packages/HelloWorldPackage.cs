5ea6c3ac4cbd28ad86d67af6179e1b09e404a1b4
Alleshka1337 <grebovod_aleksei@mail.ru>
2020-06-28 16:55:32 +0300

    fix ignored file

---
+++ Maze/Maze/Maze.Common.MazePackages/Packages/HelloWorldPackage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maze.Common.MazaPackages.Packages
+{
+    /// <summary>
+    /// Просто тестовый пакет
+    /// </summary>
+    internal class HelloWorldPackage : BaseMazePackage
+    {
+        public string Message = "HelloWorld";
+
+    }
+}
---
+++ Maze/Maze/Maze.Common.MazePackages/Packages/LoginMazePackage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Maze.Common.MazaPackages.Packages
+{
+    /// <summary>
+    /// Пакет содержащий информацию для входа
+    /// </summary>
+    internal class LoginMazePackage : BaseMazePackage
+    {
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Пароль пользователя
+        /// </summary>
+        public string Password { get; set; }
+
+        public LoginMazePackage()
+        {
+
+        }
+
+        public LoginMazePackage(string login, string password) : base()
+        {
+            this.Login = login;
+            this.Password = password;
+        }
+    }
+}
