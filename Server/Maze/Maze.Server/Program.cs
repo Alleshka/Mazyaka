888a08b0736f80a02f6282210fde1ff565b1c0d7
Alleshka <grebovod_aleksei@mail.ru>
2023-07-12 01:36:30 +0400

    Create base server

---
+++ Server/Maze/Maze.Server/Program.cs
@@ -0,0 +1,49 @@
+using Maze.Server.SignalR;
+
+namespace Maze.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSignalR();
+
+
+
+            var app = builder.Build();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<GameHub>("/game");
+            });
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Server/Maze/Maze.Server/SignalR/GameHub.cs
@@ -0,0 +1,55 @@
+﻿using Maze.Common;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Maze.Server.SignalR
+{
+    public class GameHub : Hub
+    {
+        private static Random T = new Random();
+
+        public async Task Send (MoveDirection direction)
+        {
+            string result;
+            
+            switch(direction)
+            {
+                case MoveDirection.Left:
+                    {
+                        result = "Left";
+                        break;
+                    }
+                case MoveDirection.Right:
+                    {
+                        result = "Right";
+                        break;
+                    }
+                case MoveDirection.Down:
+                    {
+                        result = "Down";
+                        break;
+                    }
+                case MoveDirection.Up:
+                    {
+                        result = "UP";
+                        break;
+                    }
+                default:
+                    {
+                        result = @"¯\_(ツ)_/¯";
+                        break;
+                    }
+            }
+
+            await Clients.All.SendAsync("Send", result);
+        }
+
+        public async Task Move(MoveDirection direction)
+        {
+            string t = ("Move to " + direction + " cell");
+            Console.WriteLine(t);
+            await Task.FromResult(0);
+            string res = T.NextDouble() > 0.5 ? "Success" : "Wall";
+            await Clients.All.SendAsync("MoveResult", t + "::" + res);
+        }
+    }
+}
