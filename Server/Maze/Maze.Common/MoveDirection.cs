e7937da3de99c8f447feaf69d24fe8bfdec72340
Alleshka <grebovod_aleksei@mail.ru>
2023-07-12 01:45:25 +0400

    fix server

---
+++ Server/Maze/Maze.Common/MoveDirection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Maze.Common
+{
+    [Flags]
+    public enum MoveDirection
+    {
+        None = 0,
+        Up = 1 << 0,
+        Right = 1 << 1,
+        Down = 1 << 2,
+        Left = 1 << 3
+    }
+}
